688d298471578ef66872e4c4ed22abbbeefd36a0
Allan <akanyijuka@gmail.com>
2020-05-19 10:40:01 +0200

    Added Tailwindcss

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import '../css/app.css';
+import '../css/main.css';
 
 class App extends Component {
     render() {
         return (
-           <div>
-
+           <div className="h-64">
+               <div className="p-4 m-4 bg-green-600">
+                <h1 className="text-2xl font-bold text-white">
+                    TAILWIND TEST
+                </h1>
+               </div>
+               <div className="p-4 m-4 bg-green-400 h-full">
+                    <h2 className="text-green-900">
+                        This is the first test!
+                    </h2>
+                   <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                       What a button!
+                   </button>
+               </div>
            </div>
         )
     }
