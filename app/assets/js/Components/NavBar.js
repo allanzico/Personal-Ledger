2282b3345e21b6c380984470b6c8cadf2aed747d
Allan <akanyijuka@gmail.com>
2020-05-22 15:54:06 +0200

    Learning branch created

--- app/assets/js/Components/NavBar.js
+++ app/assets/js/Components/NavBar.js
@@ -34,30 +34,8 @@ class NavBar extends Component{
             }}
             </AuthContext.Consumer>
             );
-
-
     }
 
 }
-// const NavBar = () => {
-//     return (
-//
-//         <div className="bg-gray-200">
-//             <div>
-//                 <div className="container mx-auto px-4">
-//                     <div className="flex items-center  md:justify-between py-2 ">
-//                         <div className="w-1/2 md:w-auto m-auto text-center text-gray-900 text-2xl font-medium">
-//                             Personal Ledger
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//
-//         </div>
-//
-//
-//
-//     );
-// }
 
 export default NavBar;
